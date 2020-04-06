8da6aba6b7477e1d7a4c26c80f7406d37429c441
ANGOUNOU <jcangounou@hotmail.fr>
2020-04-06 15:53:14 +0200

    debut preparation des routes

--- front/src/components/app.js
+++ front/src/components/app.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { app } from './app'
+import { app } from './app';
+import { media } from './media';
+import { evenement } from './evenement';
+import { typEvent } from './typEvent';
+
+/*import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { Switch } from "react-router-dom";
+import Home from "./components/home";
+import Accueil from "./components/accueil";*/
+
 
 
 class app extends React.Component {
@@ -9,13 +18,15 @@ class app extends React.Component {
 
 		return (
 
-			);
+			<div></div>
+		
+		);
 	}
 }
 
 
 ReactDOM.render( 
 
-			,
+		<app />,
 		document.getElementById('app')	
 	);
